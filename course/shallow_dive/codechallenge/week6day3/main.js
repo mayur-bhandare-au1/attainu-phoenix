1bac2d2f22952ff2afe41c8e4f7bc5a73a274428
[mayurcode] <[mayur.bhandare1994@gmail.com]>
2019-03-27 09:33:21 +0530

    CODE CH WEEK 6 DAY 3

---
+++ course/shallow_dive/codechallenge/week6day3/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var N = 10;
+var col = 1;
+var str = "";
+
+for (var i = 1; i <= N; i++) {
+
+    str += "*";
+    console.log(str);
+
+}
+
+
+for (var i = 1; i <= N; i++) {
+    var str = "";
+
+    for (var j = 1; j <= (N - i + 1); j++) { 
+        str += "*";
+
+    }
+    console.log(str);
+}
