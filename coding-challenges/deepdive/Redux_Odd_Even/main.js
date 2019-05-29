5d605380f5e776f6b2f6759abaa0f691b65b912a
mayurcodes <mayur.bhandare5@gmail.com>
2019-05-29 08:57:47 +0530

    Redux ODD EVEN

---
+++ coding-challenges/deepdive/Redux_Odd_Even/main.js
@@ -0,0 +1,46 @@
+import {createStore} from 'redux';
+
+let scoreReducer = (state,action) =>{
+	let newState = {
+		number:0,
+		isOdd:false,
+		isEven:false
+	}
+	if (state === undefined) {
+		return {newState};
+	}
+	if(action.type === "CHECK_NUMBER"){
+		newState.number = action.number;
+		if(newState.number %2 ==0){
+
+			newState.isEven = true;
+		}
+		else{
+			newState.isOdd = true;
+		}
+	}
+	return newState;
+	
+}
+
+let store = createStore(scoreReducer);
+
+store.subscribe(()=>{
+	let state = store.getState();
+	console.log("Counter ==>",state);
+});
+
+store.dispatch({
+   type: "CHECK_NUMBER",
+   number:123
+});
+
+store.dispatch({
+   type: "CHECK_NUMBER",
+   number:3
+});
+
+store.dispatch({
+   type: "CHECK_NUMBER",
+   number:4
+});
