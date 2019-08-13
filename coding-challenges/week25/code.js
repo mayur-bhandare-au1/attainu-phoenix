270094390062ffce6a5865adc498b574b6dfe368
mayurcodes <mayur.bhandare5@gmail.com>
2019-08-13 10:14:27 +0530

    code challenge

--- coding-challenges/week25/code.js
+++ coding-challenges/week25/code.js
@@ -10,10 +10,28 @@ function seq(n){
 		}
 	  arr.push(n);	
 	} 
-	console.log(arr.length);
-	return arr;
+	//console.log(arr.length);
+	return arr.length;
 }
 
-//console.log(seq(500));
-console.log(seq(1000000));
+//console.log(seq(13));
+function main(){
+	let numbers = [], max =0, maxIndex =0;
+	for(let i=1;i<1000000;i++){
+     numbers.push(seq(i));
+     
+   }	
+  for(let i=0;i<999999;i++){
+    if(numbers[i] > max)
+		{
+			maxIndex = i;
+			
+		}
+
+  }
+  console.log(maxIndex);
+}
+
+main();
+
 
